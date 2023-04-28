161f789c6063bafce995f5edcaca7a8104b8bd96
akorzunin <akorzunin123@gmail.com>
2023-04-28 23:43:15 +0300

    feat: add docker staff

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ async def root_():
 
 if __name__ == '__main__':
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True,)
+    uvicorn.run(app, host="0.0.0.0", port=8888, )
     # uvicorn main:app --reload --host 0.0.0.0 --port 8888
