1d3312fb3c3527bb92024319d6d2f057c6c02c17
Dmitriy Platonov <dplatonov@adway.team>
2023-05-12 15:46:07 +0300

    change footer styles for mobile and add icons in footer

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,22 +1,26 @@
+import { Space } from "antd";
 import { FC } from "react";
+import { Inst } from "../../icons/Inst";
+import { VK } from "../../icons/VK";
+import { WhatsApp } from "../../icons/WhatsApp";
 
 const footerTextStyle = "text-black  text-center text-xl font-bold";
 
 export const Footer: FC = () => {
   return (
     <footer className="bg-slate-300">
-      <div className="flex flex-col gap-2 max-w-[40%] mx-auto pt-4">
+      <div className="flex flex-col gap-2 pt-5">
         <p className={footerTextStyle}>+7 912 700 95 65</p>
         <p className={footerTextStyle}>terra.glamp@mail.ru</p>
         <p className="text-gray-700 text-center text-sm py-4">
           Кировская обл., Слободской р-н, п.Чирковский завод, дачное
           товарищество "Лесник"
         </p>
-        <div className="flex gap-2 justify-center">
-          <p>VK</p>
-          <p>IN</p>
-          <p>WA</p>
-        </div>
+        <Space className="justify-center">
+          <VK />
+          <Inst />
+          <WhatsApp />
+        </Space>
         <p className="text-gray-700 text-center text-sm py-4">
           © 2023 TerraGlamp. Все права защищены.
         </p>
