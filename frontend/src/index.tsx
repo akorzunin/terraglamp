6678dbd6be683b58f155e60bf86599b5d0e2e83f
akorzunin <akorzunin123@gmail.com>
2023-05-21 23:25:35 +0300

    fix: get rid of HashRouter #8

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,9 @@
-
 import * as ReactDOM from "react-dom/client";
 import * as React from "react";
 import App from "./components/app/App";
 import "./index.css";
 import "./vendor/normalize.css";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
