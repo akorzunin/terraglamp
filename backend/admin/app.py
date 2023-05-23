a1dc737074d6e939a5e80aefc724e325a1e3087f
akorzunin <akorzunin123@gmail.com>
2023-05-23 23:45:33 +0300

    feat: add admin templates w/ https upgrade #9

--- backend/admin/app.py
+++ backend/admin/app.py
@@ -2,10 +2,14 @@ from starlette_admin import BaseAdmin as Admin
 from tinydb import TinyDB
 
 from backend.admin.view import PostView
+from backend.settings import DEBUG
 
 db = TinyDB("db.json")
 
 
-admin = Admin()
+admin = Admin(
+    templates_dir="./backend/admin/templates",
+)
+
+admin.templates.env.globals["is_debug"] = lambda: DEBUG
 admin.add_view(PostView(db))
-# admin.mount_to(app)
