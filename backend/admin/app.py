efd0b776229e92ed3363c25ffffd6537de9f5c3a
akorzunin <akorzunin123@gmail.com>
2023-05-25 23:31:37 +0300

    feat: add admin home page #9

--- backend/admin/app.py
+++ backend/admin/app.py
@@ -1,4 +1,5 @@
 from starlette_admin import BaseAdmin as Admin
+from starlette_admin.views import CustomView
 from tinydb import TinyDB
 
 from backend.admin.view import PostView
@@ -9,6 +10,9 @@ db = TinyDB("db.json")
 
 admin = Admin(
     templates_dir="./backend/admin/templates",
+    index_view=CustomView(
+        label="Home", icon="fa fa-home", path="/home", template_path="home.html"
+    ),
 )
 
 admin.templates.env.globals["is_debug"] = lambda: DEBUG
